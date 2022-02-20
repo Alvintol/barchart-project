1198bd07a258148c2d26e11a1dd4c852e9a4fda8
Alvintol <alvintol+github@hotmail.com>
2022-02-20 00:27:02 +0000

    build visual basics of project

--- bar-chart.js
+++ bar-chart.js
@@ -1,3 +1,9 @@
-function drawBarChart(data, options, elements){
+//data:     an array of numbers
+//options:  object that controls width and height of chart
+//elements: DOM or jQuery element the chart will be placed into
+let options = (width, height);
 
+
+function drawBarChart(data, options, elements){
+  
 }
