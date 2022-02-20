7d49601c3b2fc2e835ebaf038907b24b90bf16fa
Alvintol <alvintol+github@hotmail.com>
2022-02-20 01:09:54 +0000

    minor code tests to see what logs

--- bar-chart.js
+++ bar-chart.js
@@ -1,9 +1,24 @@
 //data:     an array of numbers
 //options:  object that controls width and height of chart
 //elements: DOM or jQuery element the chart will be placed into
-let options = (width, height);
 
+const data = [
+  Math.floor(Math.random() * 20),
+  Math.floor(Math.random() * 20),
+  Math.floor(Math.random() * 20),
+  Math.floor(Math.random() * 20),
+  Math.floor(Math.random() * 20)];
 
-function drawBarChart(data, options, elements){
-  
+const options = {
+  width: 100,
+  height: 100
 }
+const elements = {id: "chart", class: "column"}
+
+function drawBarChart(data, options, elements) {
+  console.log(data);
+  console.log(options);
+  console.log(elements);
+}
+
+console.log(drawBarChart(data, options, elements))
